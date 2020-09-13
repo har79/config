31f64f3cec1dfb2d9e1580afdf59215e17eb610b
har79 <github@harrycameron.com>
2020-09-13 18:32:04 +0100

    Strict jest config

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,13 @@ const common = require('./common.config')
 module.exports = {
   moduleFileExtensions: common.extensions.map(s => s.substring(1)),
   moduleDirectories: common.modules,
+  collectCoverageFrom: [`${common.root}/**`],
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  },
 }
