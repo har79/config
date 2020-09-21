051dd36ced57fd096ce548909e6094c3cb8d04c6
har79 <github@harrycameron.com>
2020-09-21 01:06:38 +0100

    Switch to symlinks

--- common.config.js
+++ common.config.js
@@ -1,8 +1,9 @@
 const ts = require('./tsconfig.json')
 const build = require('./tsconfig.build.json')
+const options = require('./options.config')
 
 module.exports = {
-  withPreact: true,
+  withPreact: options.withPreact,
 
   alias: ts.compilerOptions.paths,
   extensions: ['.ts', ...(this.withPreact ? ['.tsx'] : []), '.js'],
