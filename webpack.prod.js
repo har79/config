2079423dcbe8eb0900e748ab9c86c16569698124
Harry Cameron <github@harrycameron.com>
2022-04-16 23:38:26 +0100

    update to CssMinimizerPlugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,12 @@
 const TerserJSPlugin = require('terser-webpack-plugin')
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const {merge} = require('webpack-merge')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
-    minimizer: [new TerserJSPlugin(), new OptimizeCSSAssetsPlugin()],
+    minimizer: [new TerserJSPlugin(), new CssMinimizerPlugin()],
     sideEffects: true,
   },
 })
