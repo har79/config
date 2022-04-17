b0abf180fa70e96892bd45ec7307ad391cded25b
Harry Cameron <github@harrycameron.com>
2022-04-17 00:40:45 +0000

    run prettier

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 const TerserJSPlugin = require('terser-webpack-plugin')
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const {merge} = require('webpack-merge')
 const common = require('./webpack.common.js')
 
