c730e1e492217290af1ddf66fae0d7b090041014
har79 <github@harrycameron.com>
2025-01-13 17:42:34 +0000

    fix deps

--- jest.config.js
+++ jest.config.js
@@ -10,9 +10,7 @@ export default {
   moduleFileExtensions: common.extensions.map(s => s.substring(1)),
   // Workaround https://github.com/jestjs/jest/issues/12889.
   moduleDirectories: modules,
-  transformIgnorePatterns: [
-      "<rootDir>/node_modules/(?!lodash-es)"
-  ],
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!lodash-es)'],
   collectCoverageFrom: [`${common.src}/**`],
   coveragePathIgnorePatterns: ['/testing/'],
 }
